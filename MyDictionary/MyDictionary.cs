2987a68c0bbc98e41cee4fd6ce7fd73583220442
Gürkan Yakar <data.yakar@gmail.com>
2021-02-02 23:36:27 +0300

    Add project files.

---
+++ MyDictionary/MyDictionary.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyDictionary
+{
+    class MyDictionary<Key,Value>
+    {
+        Key[] keys;
+        Value[] values;
+
+        public MyDictionary()
+        {
+            keys = new Key[0];
+            values = new Value[0];
+        }
+        public void Add(Key key, Value value)
+        {
+            Key[] tempKeyArray = keys;
+            Value[] tempValueArray = values;
+
+            keys = new Key[keys.Length + 1];
+            values = new Value[values.Length + 1];
+
+            for (int i = 0; i < tempKeyArray.Length; i++)
+            {
+                keys[i] = tempKeyArray[i];
+                values[i] = tempValueArray[i];
+            }
+            keys[keys.Length-1] = key;//parametre olarak gelen key son elemana eklenir
+            values[values.Length - 1] = value; //parametre olarak gelen value son elemana eklenir
+
+            
+        }
+        public void Print() {
+            for (int i = 0; i < keys.Length; i++)
+            {
+                Console.WriteLine("Telefon kodu : \t"+keys[i] +"\tÜlke : \t"+values[i]);
+            }
+        }
+        
+        
+    }
+}
---
+++ MyDictionary/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace MyDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<int,string> telefonKodlari = new MyDictionary<int,string>();
+            telefonKodlari.Add(1, "ABD");
+            telefonKodlari.Add(49, "Almanya");
+            telefonKodlari.Add(81, "Japonya");
+            telefonKodlari.Add(90, "Türkiye");
+
+            telefonKodlari.Print();
+
+        }
+    }
+}
